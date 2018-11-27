272c0488ec5a908fb465300a0a077a314659140f
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-11-26 16:58:09 -0800

    main classes added

---
+++ simulation.py
@@ -0,0 +1,22 @@
+class Simulation:
+    """
+    Highest level of abstraction. 
+    The main class that runs the entire simulation
+    """
+    ...
+
+class Person:
+      """
+    Represents the people 
+    that make up the population that 
+    the virus is spreading through
+    """
+    ...
+
+class Logger:
+    """
+   A helper class for logging all
+    events that happen in the simulation
+    """
+    ...
+
